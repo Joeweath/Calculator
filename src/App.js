6b867a314f2208a84eb90bb7528b8ea6665c840e
jweathington <jweathington@gmail.com>
2022-11-10 09:58:03 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Calc">
+        <div className="Screen">
+
+        </div>
+        <div className="ButtonBox">
+          <div className="1">
+            
+          </div>
+          <div className="1">
+            
+          </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div><div className="1">
+            
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
