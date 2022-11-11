e722d8846f95c6591f2db6789e4371deaeb3edd5
jweathington <jweathington@gmail.com>
2022-11-11 09:58:02 -0600

    create calculator component

--- src/App.js
+++ src/App.js
@@ -2,55 +2,7 @@
 function App() {
   return (
     <div className="App">
-      <div className="Calc">
-        <div className="Screen">
-
-        </div>
-        <div className="ButtonBox">
-          <div className="1">
-            
-          </div>
-          <div className="1">
-            
-          </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div><div className="1">
-            
-            </div>
-        </div>
-      </div>
+      
     </div>
   );
 }
---
+++ src/components/calculator/calculator.js
@@ -0,0 +1,8 @@
+import "calculator.css"
+
+const Calculator = ({props}) => {
+    return 
+    <div className="calculator">{prop}</div>;
+};
+
+export default Calculator;
